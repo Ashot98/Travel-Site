4429df55202fd28f3d49a525103bfb1527f5daf5
Ashot <ashott98@gmail.com>
2017-08-22 22:43:30 +0400

    Header, Section 1 and 2

---
+++ smartgrid.js
@@ -0,0 +1,31 @@
+var smartgrid = require('smart-grid');
+
+var settings = {
+    outputStyle: 'sass', /* less || scss || sass || styl */
+    columns: 12, /* number of grid columns */
+    offset: '30px', /* gutter width px || % */
+    container: {
+        maxWidth: '1200px', /* max-width оn very large screen */
+        fields: '30px' /* side fields */
+    },
+    breakPoints: {
+        lg: {
+            width: '1100px', /* -> @media (max-width: 1100px) */
+            fields: '30px' /* side fields */
+        },
+        md: {
+            width: '960px',
+            fields: '15px'
+        },
+        sm: {
+            width: '780px',
+            fields: '15px'
+        },
+        xs: {
+            width: '560px',
+            fields: '15px'
+        }
+    }
+};
+
+smartgrid('./sass', settings);
